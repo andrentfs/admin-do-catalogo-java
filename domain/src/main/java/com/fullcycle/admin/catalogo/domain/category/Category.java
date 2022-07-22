b9dcdb4f8359c0df54f8b361d59150dbd1307e6f
André Luiz <andre@awtecnologia.com.br>
2022-07-22 11:31:56 -0300

    Error Application UseCase

--- domain/src/main/java/com/fullcycle/admin/catalogo/domain/category/Category.java
+++ domain/src/main/java/com/fullcycle/admin/catalogo/domain/category/Category.java
@@ -1,11 +1,12 @@
 package com.fullcycle.admin.catalogo.domain.category;
 
+import com.fullcycle.admin.catalogo.domain.AggregateRoot;
+
 import java.time.Instant;
 import java.util.UUID;
 
-public class Category {
+public class Category extends AggregateRoot<CategoryID> {
 
-    private String id;
     private String Name;
     private String Description;
     private boolean active;
@@ -14,30 +15,30 @@ public class Category {
     private Instant deleteAt;
 
     private Category(
-            final String id,
-            final String name,
-            final String description,
-            final boolean active,
-            final Instant createdAt,
-            final Instant updatedAt,
-            final Instant deleteAt
+            final CategoryID anId,
+            final String aName,
+            final String aDescription,
+            final boolean isActive,
+            final Instant aCreatedDate,
+            final Instant aUpdatedDate,
+            final Instant aDeleteDate
     ) {
-        this.id = id;
-        Name = name;
-        Description = description;
-        this.active = active;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-        this.deleteAt = deleteAt;
+        super(anId);
+        Name = aName;
+        Description = aDescription;
+        this.active = isActive;
+        this.createdAt = aCreatedDate;
+        this.updatedAt = aUpdatedDate;
+        this.deleteAt = aDeleteDate;
     }
 
     public static Category newCategory(final String aName, final String aDescription, final boolean isActive){
-        final var id =  UUID.randomUUID().toString();
+        final var id =  CategoryID.unique();
         final var now = Instant.now();
         return new Category(id, aName, aDescription, isActive, now, now, null);
     }
 
-    public String getId() {
+    public CategoryID getId() {
         return id;
     }
 
